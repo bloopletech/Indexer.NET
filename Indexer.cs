7610d2ddb8aa8d8fce30180bff61d45f6a006adc
Brenton "B-Train" Fletcher <i@bloople.net>
2026-07-31 21:57:58 +1000

    Clean up and update styles; Change timestamp format

--- Indexer.cs
+++ Indexer.cs
@@ -25,20 +25,16 @@ public class Indexer(DirectoryResult directory, string url)
                     <h1>Index of {url}</h1>
                 </header>
                 <main>
-                <div class="listing">
-                    <table aria-describedby="summary">
+                    <table>
                         <thead>
                             <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Size</th>
-                                <th class="hideable">Modified</th>
-                                <th class="hideable"></th>
+                                <th class="name">Name</th>
+                                <th class="size">Size</th>
+                                <th class="modified hideable">Modified</th>
                             </tr>
                         </thead>
                         <tbody>{RenderEntries()}</tbody>
                     </table>
-                </div>
                 </main>
             </body>
         </html>
@@ -58,105 +54,69 @@ public class Indexer(DirectoryResult directory, string url)
         .Select(RenderFile);
 
     private static string RenderParent() => $"""
-        <tr class="clickable">
-            <td></td>
-            <td><a href="../">⬆️ <span class="name">..</span></a></td>
-            <td data-order="-1">-</td>
-            <td class="hideable">-</td>
-            <td class="hideable"></td>
+        <tr>
+            <td class="name"><a href="../">⬆️ <span>..</span></a></td>
+            <td class="size">—</td>
+            <td class="modified hideable">—</td>
         </tr>
         """;
 
     private static string RenderDirectory(LinkAwareDirectoryInfo info) => $"""
-        <tr class="file">
-            <td></td>
-            <td><a href="{UrlUtility.EncodeUrlPath(info.Name + "/")}">📂 <span class="name">{info.Name}</span></a></td>
-            <td data-order="-1">-</td>
-            <td class="hideable"><time datetime="{info.LastModified:O}">{info.LastModified:F}</time></td>
-            <td class="hideable"></td>
+        <tr>
+            <td class="name"><a href="{UrlUtility.EncodeUrlPath(info.Name + "/")}">📂 <span>{info.Name}</span></a></td>
+            <td class="size">—</td>
+            <td class="modified hideable"><time datetime="{info.LastModified:O}">{info.LastModified:d MMM yyy h:m:s tt}</time></td>
         </tr>
         """;
 
     private static string RenderFile(LinkAwareFileInfo info) => $"""
-        <tr class="file">
-            <td></td>
-            <td><a href="{UrlUtility.EncodeUrlPath(info.Name)}">📄 <span class="name">{info.Name}</span></a></td>
-            <td data-order="{info.Length}">{PrettySize.Bytes(info.Length)}</td>
-            <td class="hideable"><time datetime="{info.LastModified:O}">{info.LastModified:F}</time></td>
-            <td class="hideable"></td>
+        <tr>
+            <td class="name"><a href="{UrlUtility.EncodeUrlPath(info.Name)}">📄 <span>{info.Name}</span></a></td>
+            <td class="size">{PrettySize.Bytes(info.Length)}</td>
+            <td class="modified hideable"><time datetime="{info.LastModified:O}">{info.LastModified:d MMM yyy h:m:s tt}</time></td>
         </tr>
         """;
 
     private static readonly string Styles = """
-        * { padding: 0; margin: 0; }
+        * {
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+            box-sizing: border-box;
+            font-family: system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            line-height: 1.4;
+        }
+
         body {
-            font-family: sans-serif;
-            text-rendering: optimizespeed;
-            background-color: #ffffff;
+            margin: 0;
+            padding: 0;
+            min-height: 100vh;
+            font-size: 16px;
+            background-color: #fff;
+            color: #000;
         }
         a {
             color: #006ed3;
             text-decoration: none;
         }
-        a:hover,
-        h1 a:hover {
+        a:hover {
             color: #319cff;
         }
-        header,
-        #summary {
-            padding-left: 5%;
-            padding-right: 5%;
-        }
-        th:first-child,
-        td:first-child {
-            width: 5%;
-        }
-        th:last-child,
-        td:last-child {
-            width: 5%;
-        }
         header {
-            padding-top: 25px;
-            padding-bottom: 15px;
+            padding: 25px 25px 15px 25px;
             background-color: #f2f2f2;
         }
         h1 {
+            max-width: 1200px;
+            margin: 0 auto;
             font-size: 20px;
             font-weight: normal;
-            white-space: nowrap;
-            overflow-x: hidden;
-            text-overflow: ellipsis;
-            color: #999;
-        }
-        h1 a {
-            color: #000;
-            margin: 0 4px;
-        }
-        h1 a:hover {
-            text-decoration: underline;
-        }
-        h1 a:first-child {
-            margin: 0;
         }
         main {
-            display: block;
-        }
-        .meta {
-            font-size: 12px;
-            font-family: Verdana, sans-serif;
-            border-bottom: 1px solid #9C9C9C;
-            padding-top: 10px;
-            padding-bottom: 10px;
-        }
-        .meta-item {
-            margin-right: 1em;
-        }
-        #filter {
-            padding: 4px;
-            border: 1px solid #CCC;
+            padding: 0 25px 25px 25px;
         }
         table {
             width: 100%;
+            max-width: 1200px;
+            margin: 0 auto;
             border-collapse: collapse;
         }
         tr {
@@ -165,85 +125,37 @@ public class Indexer(DirectoryResult directory, string url)
         tbody tr:hover {
             background-color: #ffffec;
         }
-        th,
-        td {
-            text-align: left;
-            padding: 10px 0;
-        }
         th {
-            padding-top: 15px;
-            padding-bottom: 15px;
-            font-size: 16px;
-            white-space: nowrap;
-        }
-        th a {
-            color: black;
-        }
-        th svg {
-            vertical-align: middle;
+            padding: 15px 5px;
+            text-align: left;
         }
         td {
+            padding: 10px 5px;
+        }
+        th:first-child, td:first-child {
+            padding-left: 0;
+        }
+        th:last-child, td:last-child {
+            padding-right: 0;
+        }
+        td.name {
+            overflow-wrap: anywhere;
             white-space: nowrap;
-            font-size: 14px;
         }
-        td:nth-child(2) {
-            width: 80%;
+        td.name span {
+            white-space: normal;
         }
-        td:nth-child(3) {
-            padding: 0 20px 0 20px;
+        th.size, td.size {
+            width: 100px;
         }
-        th:nth-child(4),
-        td:nth-child(4) {
+        th.modified, td.modified {
+            width: 195px;
             text-align: right;
         }
-        td:nth-child(2) svg {
-            position: absolute;
-        }
-        td .name {
-            margin-left: 1.75em;
-            word-break: break-all;
-            overflow-wrap: break-word;
-            white-space: pre-wrap;
-        }
-        td .goup {
-            margin-left: 1.75em;
-            padding: 0;
-            word-break: break-all;
-            overflow-wrap: break-word;
-            white-space: pre-wrap;
-        }
-        .icon {
-            margin-right: 5px;
-        }
-        tr.clickable { 
-            cursor: pointer; 
-        } 
-        tr.clickable a { 
-            display: block; 
-        } 
+
         @media (max-width: 600px) {
-            * {
-                font-size: 1.06rem;
-            }
             .hideable {
                 display: none;
-            }
-            td:nth-child(2) {
-                width: auto;
-            }
-            th:nth-child(3),
-            td:nth-child(3) {
-                padding-right: 5%;
-                text-align: right;
-            }
-            h1 {
-                color: #000;
-            }
-            h1 a {
-                margin: 0;
-            }
-            #filter {
-                max-width: 100px;
             }
         }
         """;
